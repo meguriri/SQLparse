7c16452a94465c341812aec01bb1f2590ec1f64c
meguriri <meguriri1019@gmail.com>
2025-01-14 17:36:27 +0800

    add generateSQL

--- app.py
+++ app.py
@@ -1,8 +1,35 @@
 from db import Conn
-from query import q0
+from query import q0,table_q,colum_q
 from config import opt
+from sql import getTokens,getQueryTabName,getConditions,getQueryColName
 
-conn = Conn(opt.username,opt.password,opt.host,opt.port,opt.database)
-data = conn.execSQL(q0)
+def generateSQL(row_sql,conn):
+    statement = getTokens(row_sql)
+    colums=getQueryColName(statement.tokens)
+    table_name=getQueryTabName(statement.tokens)
+    conditions=getConditions(statement.tokens)
+    # print(colums)
+    # print(table_name)
+    # print(conditions)
+    tables = []
+    for colum in colums:
+        data = conn.execSQL(colum_q.format(
+            colum_name = colums[0],
+            raw_table_name =  ",".join(table_name),
+        ))
+        tables.extend(data['table_names'].tolist()[0])
 
-print(data)
+    tables = list(set(tables))
+    print(tables)
+    # 查询join关系
+
+    # 生成新的SQL
+    newSQL = ""
+    return newSQL
+    
+
+if __name__ == '__main__':
+    conn = Conn(opt.username,opt.password,opt.host,opt.port,opt.database)
+    # data = conn.execSQL(colum_q.format(colum_name='uid'))
+    r_sql = "SELECT userid FROM t1 WHERE age=22;"
+    generateSQL(r_sql,conn)
